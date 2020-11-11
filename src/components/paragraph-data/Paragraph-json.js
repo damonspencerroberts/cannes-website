8122cb68d936c53698873383020c54e7be97d7c7
Damon <damonspencerroberts@gmail.com>
2020-11-11 12:33:52 +0000

    Added paragraph components. Added json paragraph data. Configured word size per line

---
+++ src/components/paragraph-data/Paragraph-json.js
@@ -0,0 +1,9 @@
+import Paragraph from "../../container/paragraph/Paragraph"
+
+const ParagraphData = {
+    home: {
+        paragraph: "Welcome to our beautiful apartment located in Cannes on the Cote d’Azur.  We are situated only a few minutes from some of the nicest beaches in Cannes and around a 15 minute walk to the Palais des Festival. We are the ideal location for either a fabulous Summer vacation or as the perfect base for you to attend one of the numerous Congresses held every year."
+    }
+}
+
+export default ParagraphData;
---
+++ src/container/paragraph/Paragraph.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import classes from './Paragraph.module.css';
+
+const Paragraph = (props) => {
+
+    const para = props.elementParagraph;
+
+    const paraArr = para.split(' '); //props.here...
+    const splitArr = [];
+    let i, temp, chunk = 15; 
+    for (i = 0; i < paraArr.length; i+=chunk) {
+        temp = paraArr.slice(i, i+chunk);
+        splitArr.push(temp);
+    }
+
+    const newSplit = splitArr.map(e => {
+        return e.join(' ');
+    });
+
+    return (
+        <div className={classes.Paragraph}>
+            {newSplit.map(e => {
+                return <p key={e}>{e}</p>
+            })}
+        </div>
+    );
+}
+
+export default Paragraph;
