1311032f087cccab0b46cd7305188cd591b19715
Damon <damonspencerroberts@gmail.com>
2020-11-11 13:28:58 +0000

    Added logo to the top of page

---
+++ src/container/logo/Logo.js
@@ -0,0 +1,15 @@
+import { checkPropTypes } from 'prop-types';
+import React from 'react';
+import LogoPic from '../../images/bellevueLogo.png';
+import classes from './Logo.module.css';
+
+const Logo = () => {
+    return (
+        <div className={classes.Div}>
+            <a href = "/"><img src={LogoPic} alt = "logo-bellevue" className={classes.Image} /></a>
+        </div>
+        
+    );
+}
+
+export default Logo;
