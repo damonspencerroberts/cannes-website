661d6e576fdbecf88b65b64bfebd2f569a5a69b3
Damon <damonspencerroberts@gmail.com>
2020-11-12 15:10:19 +0000

    Added Hr Tag to the contact page

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Banner from "../../container/banner/Banner";
 import ContactImg from "../../images/contact/ct.jpeg";
+import HrTag from "../../container/hr-tag/Hr-tag";
 
 export default class Contact extends Component {
     render() {
@@ -10,6 +11,7 @@ export default class Contact extends Component {
                     bannerImg={ContactImg}
                     header = "Contact us"
                 />
+                <HrTag />
             </div>
         );
     }
