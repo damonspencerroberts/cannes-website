bb947d076e4ee2607ffe27622313345b780f7ade
Damon <damonspencerroberts@gmail.com>
2020-11-12 17:48:21 +0000

    changed paragraph.js to class

--- src/container/paragraph/Paragraph.js
+++ src/container/paragraph/Paragraph.js
@@ -1,47 +1,50 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Paragraph.module.css';
 
-const Paragraph = (props) => {
+class Paragraph extends Component {
+    
+    render() {
+        const para = this.props.elementParagraph;
 
-    const para = props.elementParagraph;
+        const paraArr = para.split(' '); //props.here...
+        const splitArr = [];
+        let i, temp, chunk = this.props.chunk; 
+        for (i = 0; i < paraArr.length; i+=chunk) {
+            temp = paraArr.slice(i, i+chunk);
+            splitArr.push(temp);
+        }
 
-    const paraArr = para.split(' '); //props.here...
-    const splitArr = [];
-    let i, temp, chunk = props.chunk; 
-    for (i = 0; i < paraArr.length; i+=chunk) {
-        temp = paraArr.slice(i, i+chunk);
-        splitArr.push(temp);
-    }
+        const newSplit = splitArr.map(e => {
+            return e.join(' ');
+        });
 
-    const newSplit = splitArr.map(e => {
-        return e.join(' ');
-    });
-
-    return (
-        <div className={classes.Paragraph}>
-            {newSplit.map(e => {
-                return <p key={e}>{e}</p>
-            })}
-            <br />
-            {props.links ? <div className = {classes.LinksDiv}>
-                <p>{props.linkPara}</p>
+        return(
+            <div className={classes.Paragraph}>
+                {newSplit.map(e => {
+                    return <p key={e}>{e}</p>
+                })}
                 <br />
-                <span>
-                    <a style = {{fontWeight: "bolder"}} href = {props.link1} target = "_blank" rel="noreferrer"><i className="fab fa-airbnb"></i> {props.link1Words}</a> | 
-                    <a style = {{fontWeight: "bolder"}} href = {props.link2} target = "_blank" rel="noreferrer"><i class="fas fa-globe-europe"></i> {props.link2Words}</a> | 
-                    <a style = {{fontWeight: "bolder"}} href = "#form-of-contact"><i className="fas fa-envelope"></i> Find out more</a>
-                </span>
-            </div> : null }
-            {props.findLink ? 
-                <div className = {classes.LinksDiv}>
+                {this.props.links ? <div className = {classes.LinksDiv}>
+                    <p>{this.props.linkPara}</p>
+                    <br />
                     <span>
+                        <a style = {{fontWeight: "bolder"}} href = {this.props.link1} target = "_blank" rel="noreferrer"><i className="fab fa-airbnb"></i> {this.props.link1Words}</a> | 
+                        <a style = {{fontWeight: "bolder"}} href = {this.props.link2} target = "_blank" rel="noreferrer"><i class="fas fa-globe-europe"></i> {this.props.link2Words}</a> | 
                         <a style = {{fontWeight: "bolder"}} href = "#form-of-contact"><i className="fas fa-envelope"></i> Find out more</a>
                     </span>
-                </div> : null}
-        </div>
-    );
+                </div> : null }
+                {this.props.findLink ? 
+                    <div className = {classes.LinksDiv}>
+                        <span>
+                            <a style = {{fontWeight: "bolder"}} href = "#form-of-contact"><i className="fas fa-envelope"></i> Find out more</a>
+                        </span>
+                    </div> : null}
+            </div>
+        );
+    }
 }
+
 
 export default Paragraph;
 
