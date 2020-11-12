0169eb1aaa8372969e265407c04311ba55360033
Damon <damonspencerroberts@gmail.com>
2020-11-12 18:14:00 +0000

    added banner to apt and configure gallery grow

--- src/components/apartment/Apartment.js
+++ src/components/apartment/Apartment.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
+import Banner from "../../container/banner/Banner";
+import BannerImg from "../../images/folder/apt/a7.jpg";
+import HrTag from "../../container/hr-tag/Hr-tag";
 
 export default class Apartment extends Component {
     render() {
         return(
             <div>
-                <h1>Apartment Page</h1>
+            <Banner 
+                bannerImg={BannerImg}
+                header = "Palais Bellevue"
+                highlightColor="rgba(90, 90, 90, 0.507)"
+            />
+            <HrTag />
+            
             </div>
         );
     }
