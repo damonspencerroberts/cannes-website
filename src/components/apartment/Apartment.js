f1fb45397d0e528e963b2f6c4d91ffa019395231
Damon <damonspencerroberts@gmail.com>
2020-11-12 18:26:41 +0000

    Added images json and added them to gallery component

--- src/components/apartment/Apartment.js
+++ src/components/apartment/Apartment.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import Banner from "../../container/banner/Banner";
 import BannerImg from "../../images/folder/apt/a7.jpg";
 import HrTag from "../../container/hr-tag/Hr-tag";
+import Gallery from "../../container/gallery/Gallery";
+import Images from "../images-gallery/Apt-gallery";
 
 export default class Apartment extends Component {
     render() {
@@ -13,7 +15,10 @@ export default class Apartment extends Component {
                 highlightColor="rgba(90, 90, 90, 0.507)"
             />
             <HrTag />
-            
+            <Gallery 
+                homeJson = {Images}
+            />
+            <HrTag />
             </div>
         );
     }
---
+++ src/components/images-gallery/Apt-gallery.js
@@ -0,0 +1,74 @@
+import a1 from "../../images/folder/apt/a1.jpeg";
+import a2 from "../../images/folder/apt/a2.jpeg";
+import a3 from "../../images/folder/apt/a3.jpeg";
+import a4 from "../../images/folder/apt/a4.jpeg";
+import a5 from "../../images/folder/apt/a5.jpg";
+import a6 from "../../images/folder/apt/a6.jpg";
+import a7 from "../../images/folder/apt/a7.jpg";
+import a8 from "../../images/folder/apt/a8.jpg";
+import a9 from "../../images/folder/apt/a9.jpeg";
+import a10 from "../../images/folder/apt/a10.jpeg";
+import a11 from "../../images/folder/apt/a11.jpg";
+import a12 from "../../images/folder/apt/a12.jpg";
+import a13 from "../../images/folder/apt/a13.jpg";
+import a14 from "../../images/folder/apt/a14.jpg";
+import a15 from "../../images/folder/apt/a15.jpg";
+import a16 from "../../images/folder/apt/a16.jpg";
+import a17 from "../../images/folder/apt/a17.jpg";
+
+const AptGallery = [
+    {
+        key: 0,
+        source: a1
+    },{
+        key: 1,
+        source: a2
+    },{
+        key: 2,
+        source: a3
+    },{
+        key: 3,
+        source: a4
+    },{
+        key: 4,
+        source: a5
+    },{
+        key: 5,
+        source: a6
+    },{
+        key: 6,
+        source: a7
+    },{
+        key: 7,
+        source: a8
+    },{
+        key: 8,
+        source: a9
+    },{
+        key: 9,
+        source: a10
+    },{
+        key: 10,
+        source: a11
+    },{
+        key: 11,
+        source: a12
+    },{
+        key: 12,
+        source: a13
+    },{
+        key: 13,
+        source: a14
+    },{
+        key: 14,
+        source: a15
+    },{
+        key: 15,
+        source: a16
+    },{
+        key: 16,
+        source: a17
+    }
+];
+
+export default AptGallery
