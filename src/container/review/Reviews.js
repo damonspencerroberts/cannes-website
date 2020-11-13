179993a61c497c9ba32d2bd7170383b71ddf0b75
Damon <damonspencerroberts@gmail.com>
2020-11-13 17:56:33 +0000

    Inital plans for review

---
+++ src/container/review/Reviews.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import classes from "./Review.module.css"
+
+const Reviews = (props) => {
+
+    return(
+        <div className = {classes.Review}>
+            <div className = {classes.Each}>
+                <p className = {classes.Quote}>"This apartmnet is amazing. Its excellent, nothing else beats it. I would reccomdent it to all my friends and colleagues. Thank you for a fantastic stay!"</p>
+                <p className = {classes.Name}>Dennis Koefner <span className = {classes.Nationality}>from Austria</span></p>
+            </div>
+        </div>
+    );
+
+}
+
+export default Reviews;
