7573c84be31b407b26dd15096358390743043f72
Damon <damonspencerroberts@gmail.com>
2020-11-13 13:21:20 +0000

    Added photos for cannes page

---
+++ src/components/images-gallery/Cannes-gallery.js
@@ -0,0 +1,38 @@
+import a1 from "../../images/folder/ca/a1.jpeg";
+import a2 from "../../images/folder/ca/a2.jpg";
+import a3 from "../../images/folder/ca/a3.jpg";
+import a4 from "../../images/folder/ca/a4.jpg";
+import a5 from "../../images/folder/ca/a5.jpg";
+import a6 from "../../images/folder/ca/a6.jpeg";
+import a7 from "../../images/folder/ca/a7.jpeg";
+import a8 from "../../images/folder/ca/a8.jpeg";
+
+const CannesGallery = [
+    {
+        key: 0,
+        source: a1
+    },{
+        key: 1,
+        source: a2
+    },{
+        key: 2,
+        source: a3
+    },{
+        key: 3,
+        source: a4
+    },{
+        key: 4,
+        source: a5
+    },{
+        key: 5,
+        source: a6
+    },{
+        key: 6,
+        source: a7
+    },{
+        key: 7,
+        source: a8
+    }
+];
+
+export default CannesGallery;
