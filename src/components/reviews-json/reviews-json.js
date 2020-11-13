559557031712defe81316df3ca06137513b9e30f
Damon <damonspencerroberts@gmail.com>
2020-11-13 20:42:54 +0000

    Added more reviews and configured review count

--- src/components/reviews-json/reviews-json.js
+++ src/components/reviews-json/reviews-json.js
@@ -6,19 +6,29 @@ const reviews = [
     },
     {
         key: 1,
+        quote: "Fantastic historic residence Beautiful new apartment with impeccable comfort. A view of dreams… Quiet.",
+        person: "Claudine from France"
+    },
+    {
+        key: 2,
+        quote: "Beautiful period building, beautiful very well maintained garden and the apartment is completely renovated with high quality materials and equipment. Painted English wooden furniture giving a good overall consistency. The distance from the street allows a quiet stay. The view of the Rothschild villa is beautiful. Visitors can park in the building. Bathroom very comfortable, very nice marble furniture and wood and walk in shower also offering great comfort.",
+        person: "Childiric from France"
+    },
+    {
+        key: 3,
         quote: "Beautiful building and apartment with a wonderful view in a quiet neighborhood near the beach. The host and manger are very service minded.",
         person: "Jonas from Sweden"
     },
     {
-        key: 2,
-        quote: "Beautiful area and lots of things to do. Best place for a vacation!",
+        key: 4,
+        quote: "A very tastefully refurbished apartment in an historical building set in lovely communal gardens. Easy walk to the coast and Cannes town with it’s many restaurants and amenities.",
         person: "Karen from the UK"
     },
     {
-        key: 3,
-        quote: "Fantastci location with a very organised host. Would reccomend.",
+        key: 5,
+        quote: "",
         person: "Theresa from the UK"
-    }  
+    } 
 ];
 
 
