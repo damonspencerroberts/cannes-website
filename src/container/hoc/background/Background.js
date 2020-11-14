55fd9ccc43d8413070a6fdc1d8f21ec62ad44896
Damon <damonspencerroberts@gmail.com>
2020-11-14 17:07:47 +0000

    Added burger menu and sidebar for mobile

--- src/container/hoc/background/Background.js
+++ src/container/hoc/background/Background.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import classes from './Background.module.css';
 
-const Background = (props) => {
-    return (
-        <div className = {classes.Background} onClick = {props.clicked}>
-            {props.children}
-        </div>
-    );
-}
+const Background = (props) => props.show ? <div className = {classes.Background} onClick = {props.clicked} > {props.children} </div> : null
 
 export default Background;
