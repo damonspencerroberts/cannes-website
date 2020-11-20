729c453aabf81631edaa1e02747b04a3ecc017ce
Damon <damonspencerroberts@gmail.com>
2020-11-20 13:06:35 +0000

    Added more Meta for SEO purposes

--- src/components/apartment/Apartment.js
+++ src/components/apartment/Apartment.js
@@ -7,12 +7,26 @@ import Images from "../images-gallery/Apt-gallery";
 import Paragraph from "../../container/paragraph/Paragraph";
 import ParagraphData from "../paragraph-data/Paragraph-json";
 import ContactForm from "../../container/contact-form/Contact-form";
+import {Helmet} from "react-helmet";
+
 
 export default class Apartment extends Component {
     render() {
         return(
             <div>
-                <Banner 
+                <Helmet>
+                    <title>Palais Bellevue Apartment Cannes</title>
+                    <meta name="Cannes Palais Bellevue Apartment" content="Luxury Rental Apartment in Cannes." />
+                    <meta name="author" content="Michelle Roberts" />
+                    <meta name="description" content="Our apartment is located in the beautiful Palais 
+                        Bellevue building. Palais Bellevue was originally constructed in 1858 to cater to the 
+                        many English tourists who had begun to discover the delights of this sleepy fishing village. 
+                        When you step inside the marble lobby, it is easy to imagine a string quartet playing in the 
+                        corner, whilst fashionable tourists stroll around the grounds, taking in the beautiful surroundings 
+                        and sea air." />
+                    <meta name="robots" content="index, follow" />
+               </Helmet> 
+               <Banner 
                     bannerImg={BannerImg}
                     header = "Palais Bellevue"
                     highlightColor="rgba(90, 90, 90, 0.507)"
--- src/components/beach/Beach.js
+++ src/components/beach/Beach.js
@@ -7,11 +7,25 @@ import ParagraphData from "../paragraph-data/Paragraph-json";
 import Images from "../images-gallery/Beach-gallery";
 import HrTag from "../../container/hr-tag/Hr-tag";
 import BannerImg from "../../images/banner/b4.jpg";
+import {Helmet} from "react-helmet";
 
 export default class Beach extends Component {
     render() {
         return(
             <div>
+                <Helmet>
+                    <title>World Famous Beach</title>
+                    <meta name="Cannes Site" content="World Famous Cannes Beach" />
+                    <meta name="author" content="Michelle Roberts" />
+                    <meta name="description" content="Our apartment is located 4 
+                        minutes from the Plage du Midi beaches. All year round you will 
+                        find people playing volleyball or just relaxing on the white sand 
+                        beaches. There is a beautiful pedestrian promenade where you can walk 
+                        into the centre of Cannes in one direction and to Cannes La Bocca in 
+                        the other. This is popular with joggers and walkers alike."
+                    />
+                    <meta name="robots" content="index, follow" />
+                </Helmet>
                 <Banner 
                     bannerImg={BannerImg}
                     header = "La Plage"
